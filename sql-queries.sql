165f98a166c1463f7dad7c6d0f999852af85ad04
vikrant behal <vikbehal@gmail.com>
2019-01-30 00:42:55 -0500

    Create sql-queries.sql

---
+++ sql-queries.sql
@@ -0,0 +1,11 @@
+/******
+Basic SQL operations
+
+Preparing environment. You may skip them if you have access to any SQL
+1. Please download required version from https://www.sqlite.org/download.html
+2. Next, install SQLLiteStudio from here https://sqlitestudio.pl/index.rvt?act=download
+
+You can refer http://www.sqlitetutorial.net/download-install-sqlite/ to get more help!
+
+Congratulations! You have an environment to play
+*********/
