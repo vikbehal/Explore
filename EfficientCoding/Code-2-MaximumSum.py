072c5cb7cf1f0852e78f1391ff22ef020688bc29
vikrant behal <vikbehal@gmail.com>
2021-08-19 19:03:29 -0400

    Add files via upload

--- EfficientCoding/Code-2-MaximumSum.py
+++ EfficientCoding/Code-2-MaximumSum.py
@@ -1,15 +1,18 @@
 def findMaxSum(input_list):
-    previous = input_list[0]
-    current = max(input_list[0], input_list[1])
+    if len(input_list) == 0:
+        return None
+    elif len(input_list) == 1:
+        return input_list[0]
+    else:
+        previous = input_list[0]
+        current = input_list[1]
 
-    for val in input_list[2:]:
-        val_max = max(current, previous + val)
-        print(val_max)
-        previous = current
-        current = val_max if val_max > val else val
+        for val in input_list[2:]:
+            val_max = val + previous
+            previous = current
+            current = val_max if val_max > val else val
 
-    return current
-
+        return current if current > previous else previous
 
 input_list = "-2,1,3,-4,5"
 input_list = input_list.split(",")
